c0690f4683044bdbf70046f3ba9c4cadf3c87670
Zheng <905618355@qq.com>
2018-12-11 09:56:51 +0800

    当面付

--- face-pay/f2fpay/qrpay_test.php
+++ face-pay/f2fpay/qrpay_test.php
@@ -287,7 +287,7 @@ ul,ol{
 				<li class="last">3、确认完成</li>
             </ol>
         </div>
-        <form name=alipayment action="" method=post><!-- target="_blank"-->
+        <form name=alipayment action="" method="post" target="_blank"><!-- target="_blank"-->
             <div id="body" style="clear:left">
                 <dl class="content">
 					<dt>商户订单号：</dt>
